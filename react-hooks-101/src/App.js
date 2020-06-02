e3443953c90ffb81a16e82f56bdfc795c7f8e766
山田将史 <masashisama94@gmail.com>
2020-06-02 23:09:42 +0900

    useState

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () =>{
+const App = () => {
+
+  const [ count, setCount ] = useState( 0 ) //　配列を返す
+  const increment = () => setCount( count + 1 )
+  const decrement = () => setCount( count - 1 )
+
+  const increment2 = () => setCount( previousCount => previousCount + 1 )
+  const decrement2 = () => setCount( previousCount => previousCount - 1 )
+
+  const reset = () => setCount( 0 )
+
+  const double = () => setCount( count * 2 )
+
+  const wari3 = () => setCount( previousCount =>
+    previousCount % 3 === 0 ? previousCount / 3 : previousCount
+  )
+
   return (
-    <div>
-        this is a template
-    </div>
+    <>
+      <div>count :{ count }</div>
+      <div>
+        <button onClick={ increment }>+1</button>
+        <button onClick={ decrement }>-1</button>
+      </div>
+      <div>
+        <button onClick={ increment2 }>+1</button>
+        <button onClick={ decrement2 }>-1</button>
+      </div>
+      <div>
+        <button onClick={ reset }>reset</button>
+        <button onClick={ double }>x2</button>
+        <button onClick={ wari3 }>3の倍数の時だけ3でわる</button>
+      </div>
+    </>
   );
 }
 
