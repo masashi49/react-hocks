c2dcc209c6faed7e26e75253cc7616b61548df9d
yamadamasashi <shisama94@gmail.com>
2020-06-13 23:48:46 +0900

    コメント追加

--- react-hooks-101/src/components/App.js
+++ react-hooks-101/src/components/App.js
@@ -8,10 +8,9 @@ import reducer from "../reducers";
 
 const App = () => {
   const [ state, dispatch ] = useReducer( reducer, [] ) // useReducerの第一引数にはreducerの実態、第二引数には初期状態、第三は初期化時のみ発火する関数」
-
   return (
     <div className="container-fluid">
-      <EventForm state={ state } dispatch={ dispatch } />
+      <EventForm state={ state } dispatch={ dispatch } />{/*stateをpropとして読ませることで、1つのstateを共有する。こうしないと別々のstateを定義してデータが反映されないというバグになる。*/ }
       <Events state={ state } dispatch={ dispatch } />
     </div>
   );
