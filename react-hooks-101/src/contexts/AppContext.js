a9019b433b4aecb2fe9cab34e5328ea6b33c8cfb
yamadamasashi <shisama94@gmail.com>
2020-06-16 23:39:28 +0900

    create one react context

---
+++ react-hooks-101/src/contexts/AppContext.js
@@ -0,0 +1,9 @@
+//　データを提供したいコンポーネント（provider:提供）
+//　そのデータを使用したいコンポーネント(consumer: 使用者)
+// 共有したいデータを使痛い時、contextを使用する。
+
+import { createContext } from "react"
+
+const AppContext = createContext()
+
+export default AppContext
