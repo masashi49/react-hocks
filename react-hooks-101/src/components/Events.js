0a7a14c2b51c851d57b052e08bb04642bd82f262
yamadamasashi <shisama94@gmail.com>
2020-06-16 23:56:09 +0900

    useContextde

--- react-hooks-101/src/components/Events.js
+++ react-hooks-101/src/components/Events.js
@@ -1,14 +1,12 @@
-import React from "react"
+import React, { useContext } from "react"
 import Event from "./Event"
 import AppContext from "../contexts/AppContext"
 
 const Events = ( { state, dispatch } ) => {
+    const value =  useContext ( AppContext ) 
     return (
         <>
-            <AppContext.Consumer>
-                { value => { return <div>{ value }</div> } }
-            </AppContext.Consumer>
-
+            <div>{ value }</div>
             <h4>イベント作成フォーム</h4>
             <table className="table table-hover mt-4">
                 <thead>
