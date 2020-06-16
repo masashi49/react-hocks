a32c36a7c9d731ec08142fa69b095a8e07687fb7
yamadamasashi <shisama94@gmail.com>
2020-06-16 23:53:35 +0900

    consumerwo]

--- react-hooks-101/src/components/App.js
+++ react-hooks-101/src/components/App.js
@@ -3,16 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventForm from "./EventForm"
 import Events from "./Events"
+import AppContext from "../contexts/AppContext"
 import reducer from "../reducers";
 
+console.log( AppContext )
 
 const App = () => {
   const [ state, dispatch ] = useReducer( reducer, [] ) // useReducerの第一引数にはreducerの実態、第二引数には初期状態、第三は初期化時のみ発火する関数」
   return (
-    <div className="container-fluid">
-      <EventForm state={ state } dispatch={ dispatch } />{/*stateをpropとして読ませることで、1つのstateを共有する。こうしないと別々のstateを定義してデータが反映されないというバグになる。*/ }
-      <Events state={ state } dispatch={ dispatch } />
-    </div>
+    <AppContext.Provider value="hello i am a provieder">
+      <div className="container-fluid">
+        <EventForm state={ state } dispatch={ dispatch } />{/*stateをpropとして読ませることで、1つのstateを共有する。こうしないと別々のstateを定義してデータが反映されないというバグになる。*/ }
+        <Events state={ state } dispatch={ dispatch } />
+      </div>
+    </AppContext.Provider>
   );
 }
 
--- react-hooks-101/src/components/Events.js
+++ react-hooks-101/src/components/Events.js
@@ -1,9 +1,14 @@
 import React from "react"
 import Event from "./Event"
+import AppContext from "../contexts/AppContext"
 
 const Events = ( { state, dispatch } ) => {
     return (
         <>
+            <AppContext.Consumer>
+                { value => { return <div>{ value }</div> } }
+            </AppContext.Consumer>
+
             <h4>イベント作成フォーム</h4>
             <table className="table table-hover mt-4">
                 <thead>
